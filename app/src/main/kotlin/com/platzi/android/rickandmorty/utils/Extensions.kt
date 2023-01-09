5467821416073747669f48177dff473768b06562
Osvel Alvarez <oalvarez@sisinfo.com.uy>
2023-01-09 15:08:04 -0300

    [FIX] Gradle update

--- app/src/main/kotlin/com/platzi/android/rickandmorty/utils/Extensions.kt
+++ app/src/main/kotlin/com/platzi/android/rickandmorty/utils/Extensions.kt
@@ -62,11 +62,11 @@ inline fun <reified T : ViewModel> FragmentActivity.getViewModel(crossinline fac
 }
 
 inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
-    SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
+    SDK_INT >= 33 -> getParcelableExtra(key)
     else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
 }
 
 inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
-    SDK_INT >= 33 -> getParcelable(key, T::class.java)
+    SDK_INT >= 33 -> getParcelable(key)
     else -> @Suppress("DEPRECATION") getParcelable(key) as? T
 }
